25e4222f3d14d44d039bd2305cd84988c00fd94e
SoundharyaDGRT <soundnext123@gmail.com>
2021-07-06 12:04:26 +0530

    Create insertnode.cpp

---
+++ Problem Solving/Data Structures/04 Insert a Node at the Tail of the Linked List/insertnode.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void InsertLast(int x)
+{
+    struct Node *t, *last;
+    t = new Node;
+    t->data = x;
+    t->next = NULL;
+    if (first == NULL)
+    {
+        first = last = t;
+    }
+    else
+    {
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+}
+
+int main()
+{
+
+    //create(A, 0);
+    InsertLast(141);
+    InsertLast(302);
+    InsertLast(164);
+    InsertLast(503);
+    InsertLast(474);
+    display(first);
+    return 0;
+}
+
+/*
+Output:
+141 302 164 503 474
+*/
